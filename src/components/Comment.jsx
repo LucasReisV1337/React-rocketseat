1274ebf3d1ddeba99b4c02e12cb0a8380323a8c0
LucasReisV1337 <lucasreisvasconcelos@icloud.com>
2022-12-27 10:55:00 -0300

    A

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,22 +1,35 @@
-import styles from './Comment.module.css';
+import styles from "./Comment.module.css";
+import { useState } from "react";
 
-import { ThumbsUp, Trash } from 'phosphor-react';
-import { Avatar } from './Avatar';
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
   function handleDeleteComment() {
     onDeleteComment(content);
   }
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://lh3.googleusercontent.com/a-/ACNPEu_92GLWk2LmPYpn0wtiFLw_wShZGwh58abydWWE3A=s88-w88-h88-c-k" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://lh3.googleusercontent.com/a-/ACNPEu_92GLWk2LmPYpn0wtiFLw_wShZGwh58abydWWE3A=s88-w88-h88-c-k"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
               <strong>Maurício Kit</strong>
-              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+                Cerca de 1h atrás
+              </time>
             </div>
 
             <button onClick={handleDeleteComment} title="Deletar comentário">
@@ -25,16 +38,15 @@ export function Comment({ content, onDeleteComment }) {
           </header>
 
           <p>{content}</p>
-
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Curtir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
     </div>
-  )
+  );
 }
